037266e6ba5ea5b4a998b546779dea913c86b7e8
Tom Brendlé <tom.brendle@student.ecp.fr>
2016-04-09 15:09:33 +0200

    equals on card type

--- src/core/CardFactory.java
+++ src/core/CardFactory.java
@@ -11,13 +11,13 @@ public class CardFactory {
 	 * @return an fidelity card (object) of the class given as a paramater
 	 */
 	public static FidelityStrategy create(String s){
-			if(s =="Basic")
-				return new BasicCard();
-			else if (s=="Point")
-				return new PointCard();
-			else if (s=="Lottery")
-				return new LotteryCard();
-			else
-				throw new IllegalArgumentException("Can't create fidelity card for string : " + s);
+		if(s.equals("Basic"))
+			return new BasicCard();
+		else if (s.equals("Point"))
+			return new PointCard();
+		else if (s.equals("Lottery"))
+			return new LotteryCard();
+		else
+			throw new IllegalArgumentException("Can't create fidelity card for string : " + s);
 	}
 }
