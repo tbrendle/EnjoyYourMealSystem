7bccacf897ecdad54e9ac1ab264185f7b4845610
Tom Brendlé <tom.brendle@student.ecp.fr>
2016-04-09 15:20:35 +0200

    Typos

--- src/clui/LoginView.java
+++ src/clui/LoginView.java
@@ -11,17 +11,19 @@ public class LoginView extends AbstractView {
 		if(user instanceof Admin){
 			System.out.println(((User) user).getUserName()+ " successfully logged in as admin");
 			System.out.println("You can type : ");
-			System.out.println("addMeal<MealName, price>: to create a meal");
+			System.out.println("createMeal<MealName, price>: to create a meal");
 			System.out.println("insertOffer<mealName, price> : to insert offer");
-			System.out.println("showMeals<> to see the available meals");
-			System.out.println("showMeals<Sorting Strategy> to see stats about meal");
+			System.out.println("showMeal<> to see the available meals");
+			System.out.println("showMeal<Sorting Strategy> to see stats about meal");
 			System.out.println("associateCard <username, cardType> to associate a fidelity card to an user account");
 			System.out.println("associateAgreement <username, (y/n)> to set a user contact agreement");
 			System.out.println("addContactInfo<username, contactInfo, contactType> to add a contact info type to your account"); 
+			System.out.println("notifyAd<message, mealName, price> : to set a promotion and notify the users");
+			System.out.println("notifyBirthday<> : To notify all users that it is their birthday");
 		} else if (user instanceof Customer) {
 			System.out.println("Hello, "+ ((User) user).getFullName());
 			System.out.println("You can type : ");
-			System.out.println("showMeals<> to see the available meals");
+			System.out.println("showMeal<> to see the available meals");
 			System.out.println("selectMeal <mealName, quantity> to add a meal to your order");
 			System.out.println("associateCard <cardType> to associate a fidelity card to your account");
 			System.out.println("associateAgreement <(y/n)> to set your contact agreement");
