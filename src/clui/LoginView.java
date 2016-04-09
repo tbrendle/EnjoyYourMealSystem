ccc4a3cefbb0007abc82de435cd1d68dc892bb9a
Tom Brendlé <tom.brendle@student.ecp.fr>
2016-04-09 15:15:39 +0200

    admin view

--- src/clui/LoginView.java
+++ src/clui/LoginView.java
@@ -10,6 +10,14 @@ public class LoginView extends AbstractView {
 	public void show(Object user) {
 		if(user instanceof Admin){
 			System.out.println(((User) user).getUserName()+ " successfully logged in as admin");
+			System.out.println("You can type : ");
+			System.out.println("addMeal<MealName, price>: to create a meal");
+			System.out.println("insertOffer<mealName, price> : to insert offer");
+			System.out.println("showMeals<> to see the available meals");
+			System.out.println("showMeals<Sorting Strategy> to see stats about meal");
+			System.out.println("associateCard <username, cardType> to associate a fidelity card to an user account");
+			System.out.println("associateAgreement <username, (y/n)> to set a user contact agreement");
+			System.out.println("addContactInfo<username, contactInfo, contactType> to add a contact info type to your account"); 
 		} else if (user instanceof Customer) {
 			System.out.println("Hello, "+ ((User) user).getFullName());
 			System.out.println("You can type : ");
